1a0ee6409e161a82ec2eeda5986e10c1e11b7059
tpal-dev <tpal.developer@gmail.com>
2021-08-30 12:53:25 +0200

    add weather model

---
+++ lib/features/weather/business_logic/cubit/weather_state.dart
@@ -0,0 +1,10 @@
+part of 'weather_cubit.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+}
+
+class WeatherInitial extends WeatherState {
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/features/weather/data/models/weather.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+class Weather {
+  const Weather({
+    required this.id,
+    required this.main,
+    required this.description,
+    required this.icon,
+  });
+
+  final int id;
+  final String main;
+  final String description;
+  final String icon;
+
+  factory Weather.fromRawJson(String str) =>
+      Weather.fromJson(json.decode(str)['weather']);
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Weather.fromJson(Map<String, dynamic> json) => Weather(
+        id: json["id"],
+        main: json["main"],
+        description: json["description"],
+        icon: json["icon"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "main": main,
+        "description": description,
+        "icon": icon,
+      };
+}
