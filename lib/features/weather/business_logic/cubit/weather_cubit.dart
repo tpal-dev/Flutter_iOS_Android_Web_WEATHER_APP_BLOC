ce5ae7c7863a1d0930dc6515cc296f342d04f989
tpal-dev <tpal.developer@gmail.com>
2021-08-30 15:29:30 +0200

    add weather state cubit

--- lib/features/weather/business_logic/cubit/weather_cubit.dart
+++ lib/features/weather/business_logic/cubit/weather_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_app/features/weather/data/models/weather.dart';
 
 part 'weather_state.dart';
 
--- lib/features/weather/business_logic/cubit/weather_state.dart
+++ lib/features/weather/business_logic/cubit/weather_state.dart
@@ -2,9 +2,42 @@ part of 'weather_cubit.dart';
 
 abstract class WeatherState extends Equatable {
   const WeatherState();
+
+  @override
+  List<Object> get props => [];
 }
 
 class WeatherInitial extends WeatherState {
+  const WeatherInitial();
+}
+
+class WeatherFetchInProgress extends WeatherState {
+  const WeatherFetchInProgress();
+}
+
+class WeatherFetchSuccess extends WeatherState {
+  final Weather weather;
+
+  const WeatherFetchSuccess({
+    required this.weather,
+  });
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [weather];
+
+  @override
+  String toString() {
+    return 'WeatherFetchSuccess{weather: $weather}';
+  }
+}
+
+class WeatherFetchFailure extends WeatherState {
+  final String errorMessage;
+
+  const WeatherFetchFailure({
+    required this.errorMessage,
+  });
+
+  @override
+  List<Object> get props => [errorMessage];
 }
